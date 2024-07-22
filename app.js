cd8d70585e18872a9de5df18753ad291569fe732
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-07-22 22:10:20 +0700

    crud

--- app.js
+++ app.js
@@ -1,10 +1,79 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
+app.use(express.json())
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'Hello from server!', app: 'Duck'})
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'))
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+})
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID.'
+    })
+  }
+  const tour = tours.find(el => el.id === id)
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+})
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({id: newId}, req.body)
+  tours.push(newTour)
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID.'
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: ''
+    }
+  })
+})
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID.'
+    })
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
 })
 
 app.listen(port, () => {
