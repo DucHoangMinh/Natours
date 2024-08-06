d19a00f8ab4f91aad0b4402a252e8d66097f7457
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-08-06 10:29:19 +0700

    refactor

--- models/tourModels.js
+++ models/tourModels.js
@@ -6,11 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Name is required'],
     unique: [true, 'Name is unique']
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Duration is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Max Group Size is required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is required'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
   },
-  price: Number
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // Remove white space
+    required: [true, 'Summary is required'],
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Image cover is required'],
+  },
+  images: [String], // Array of String
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
