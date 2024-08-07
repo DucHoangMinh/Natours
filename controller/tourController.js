fdd70ff0cc7a7549d08e957f2c45864a9e16cadf
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-08-07 16:08:58 +0700

    refactor sort, filter, limit, pagintaion feature to a base class

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,55 +1,20 @@
 const fs = require('fs')
 const Tour = require('./../models/tourModels')
+const APIFeatures = require('../utils/apiFeatures')
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8'))
 
+
+
 exports.getAllTours = async (req, res) => {
   try {
-    // 1.Filtering
-    const queryObject = {...req.query}
-    const excludedField = ['page', 'sort', 'limit', 'fields']
-    excludedField.forEach(el => delete queryObject[el])
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const tours = await features.query
 
-    // 2. Advanced filtering (<=, >=)
-    let queryStr = JSON.stringify(queryObject)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-    let query = Tour.find(JSON.parse(queryStr))
-
-    // 3. Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt')
-    }
-
-    // 4. Fields limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v')
-    }
-
-    // 5. Pagination
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1)*limit
-    query = query.skip(skip).limit(limit)
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments()
-      if (skip >= numTours) {
-        throw new Error('This page is not exist')
-      }
-    }
-
-    const tours = await query
-    // query.sort().select().skip().limit()
-    // const tours = await Tour.find()
-    //   .where('duration').equals(5)
-    //   .where('difficulty').equals('easy')
     res.status(200).json({
       status: 'success',
       results: tours.length,
