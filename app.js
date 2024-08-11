98e1431c58e56a991574f3d4d95c1dc315f9d330
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-08-11 14:07:57 +0700

    Handling error middleware part 1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 
 const tourRouter = require('./route/tourRoutes')
 const userRouter = require('./route/userRoutes')
+const e = require('express');
 
 dotenv.config({ path: './config.env' })
 
@@ -26,10 +27,26 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `No endpoints found for ${req.originalUrl}`
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `No endpoints found for ${req.originalUrl}`
+  // })
+  const err = new  Error('Not Found This Router Handler')
+  err.status  = 'fail'
+  err.statusCode = 404
+
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  // Check if err has statusCode or status or not
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   })
 })
 
