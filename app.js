b65f853e1cbc957acf1ba833eff0d75e899f5468
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-08-11 14:29:50 +0700

    Refactor error handling by writing to a class

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv')
 const fs = require('fs')
 const morgan = require('morgan')
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controller/errorController')
+
 const tourRouter = require('./route/tourRoutes')
 const userRouter = require('./route/userRoutes')
 const e = require('express');
@@ -28,26 +31,9 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `No endpoints found for ${req.originalUrl}`
-  // })
-  const err = new  Error('Not Found This Router Handler')
-  err.status  = 'fail'
-  err.statusCode = 404
-
-  next(err)
+  next(new AppError(`No endpoints found for ${req.originalUrl}`))
 })
 
-app.use((err, req, res, next) => {
-  // Check if err has statusCode or status or not
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler)
 
 module.exports = app
